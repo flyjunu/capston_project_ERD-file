9300e1d6910a4c549e9ee5d88ec60e5dc4c6e6dc
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-09-24 22:39:04 +0900

    Update select_briefly_profile.sql

--- 기능구현/내 정보 조회/select_briefly_profile.sql
+++ 기능구현/내 정보 조회/select_briefly_profile.sql
@@ -19,15 +19,38 @@ AND B.university_id = A.user_university_id;
 -- (1) 조회를 위한 인덱스 생성
 create index user_departments_idx01 on user_departments(user_id);
 -- (2) 조회 SQL 
-SELECT /*+ leading (A B C) use_nl(B) use_nl(C) index(A) index(B) index(C)*/
-C.department_name AS 학과명, B.major_type AS 전공_분류
-FROM users_t A, user_departments B, departments C 
-WHERE A.user_id = :user_id
-and B.user_id = A.user_id
-and C.department_id = B.department_id
-;  
+SELECT /*+ leading (A B) use_nl(B) index(A) index(B)*/
+B.department_name AS 학과명, A.major_type AS 전공_분류
+FROM user_departments A, departments B 
+where A.user_id = :user_id
+and B.department_id = A.department_id
 
--- (3) 유저 기술 조회
+-- 3. 유저 기술 조회
+select /*+ index(B) */ 
+    B.skill_name as 기술명
+from user_skill B
+where B.user_id = :user_id;
 
+-- 4. 유저 자격증 조회
+-- (1) 인덱스 생성 
+create index user_certificates_info_idx01 on user_certificates_info(user_id)
 
+-- (2) 조회 SQL
+select /*+ leading(A B) use_nl(B) index(A) index(B)*/ 
+    A.issue_date as 취득일, B.certificate_name as 자격증명, 
+    NVL(TO_CHAR(A.expiration_date), '기한없음') as 자격만료일, A.certificate_number as 자격증번호,
+    NVL(TO_CHAR(A.score), '-') as 자격증점수 
+from user_certificates_info A, certificates B 
+where A.user_id = :user_id
+and B.certificate_id = A.user_certificate_id;
+
+-- 5. 유저 대/내외 활동 내역 조회
+-- (1) 인덱스 생성 
+create index user_activities_idx01 on user_activities(user_id);
+
+-- (2) 조회 SQL
+select activity_type as 활동_종류, activity_name as 활동명, start_date as 활동시작일,
+    end_date as 활동종료일, activity_description as 활동상세내역
+from user_activities    
+where user_id = :user_id;
 
