a4b433e86482b6c3b7ccb0f4db685066f0e6b02b
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-09-24 14:52:32 +0900

    Create select_main_profile.sql

---
+++ 기능구현/프로필조회/select_main_profile.sql
@@ -0,0 +1,12 @@
+-- 메인 화면에서 표기될 간략한 프로필
+-- 클릭시 세부 프로필 확인가능
+SELECT
+    A.user_name as 유저명,
+    A.user_age as 나이,
+    A.user_gender as 성별,
+    A.user_state as 유저상태_분류,
+    NVL(TO_CHAR(A.user_grade), '없음') as 유저_학년,
+    B.university_name as 대학명,
+FROM users_t A, universities B
+WHERE A.user_id = :user_id
+AND A.user_university_id = B.university_id; 
