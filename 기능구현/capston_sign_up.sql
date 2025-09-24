c78912d77a7b12f44a37a7563aca7242c7b1edd2
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-09-24 13:51:08 +0900

    Add files via upload

---
+++ 기능구현/capston_sign_up.sql
@@ -0,0 +1,32 @@
+-- 1. 회원 가입 기능구현(아래 6개의 SQL은 동시에 실행되어야함. 
+-- 아래코드는 모두 시퀸스를 만든 후에야 정상작동 함으로 이를 유의할 것.
+
+-- (1) user_t 테이블에 기본정보 삽입
+INSERT INTO users_t (
+    user_id, user_name, user_email, user_password, user_age, user_gender,
+    user_state, user_grade, user_university_id, user_roadmap,
+    user_credit_avg, user_major_credits_avg
+) VALUES (
+    users_seq.NEXTVAL,
+    :user_name, :user_email, :user_password, :user_age, :user_gender,
+    :user_state, :user_grade, :user_university_id, :user_roadmap,
+    :user_credit_avg, :user_major_credits_avg
+);
+
+-- (2) 유저 희망 직업 테이블에 데이터 삽입
+INSERT INTO user_job_preferences(user_id, hope_job_id) VALUES (users_seq.CURRVAL, :hope_job_id);
+
+-- (3) 유저 자격증 테이블에 데이터 삽입
+INSERT INTO user_certificates (user_certificate_id, user_id, certificate_id, issue_date, expiration_date, certificate_number, score)
+    VALUES (user_cert_seq.NEXTVAL, :user_id , :cert_id, :issue_date, :exp_date, :cert_num, :cert_score);
+
+-- (4) 유저 학과 테이블에 데이터 삽입
+INSERT INTO user_departments (user_id, department_id, major_type) VALUES (:user_id, :dept_id, :major_type);
+
+-- (5) 유저 기술 테이블에 데이터 삽입
+INSERT INTO user_skill (user_id, skill_name) VALUES (:user_id, :skill_name);
+
+-- (6) 유저 대/내외 활동 내역에 데이터 삽입
+INSERT INTO user_activities ( activity_id, user_id, activity_type, activity_name, start_date, end_date, activity_description ) 
+VALUES (activities_seq.NEXTVAL, :user_id, :activity_type, :activity_name, :start_date, :end_date, :activity_description );
+
