ac6663f534eb259e8d1990e6269fc2a958bb6813
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-09-24 14:53:22 +0900

    Update select_main_profile.sql

--- 기능구현/프로필조회/select_main_profile.sql
+++ 기능구현/프로필조회/select_main_profile.sql
@@ -6,7 +6,7 @@ SELECT
     A.user_gender as 성별,
     A.user_state as 유저상태_분류,
     NVL(TO_CHAR(A.user_grade), '없음') as 유저_학년,
-    B.university_name as 대학명,
+    B.university_name as 대학명
 FROM users_t A, universities B
 WHERE A.user_id = :user_id
 AND A.user_university_id = B.university_id; 
