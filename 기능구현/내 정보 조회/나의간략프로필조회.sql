a154177a337b5909907c683638f3d55ab02543fa
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-10-07 21:26:40 +0900

    Create 나의간략프로필조회.sql

---
+++ 기능구현/내 정보 조회/나의간략프로필조회.sql
@@ -0,0 +1,8 @@
+-- 메인화면의 나의 간략 프로필 
+
+select A.user_name AS 이름, A.user_grade AS 학년, B.job_name AS 희망직종
+from users_t A, job_categories B, user_job_preferences C
+where A.user_id = :user_id
+and C.user_id = A.user_id
+and B.job_id = C.hope_job_id;
+
