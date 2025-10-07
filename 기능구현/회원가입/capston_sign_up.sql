125dd12c025e353cdcd6ce7504af5abaeaae4928
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-10-07 21:05:16 +0900

    Update capston_sign_up.sql

--- 기능구현/회원가입/capston_sign_up.sql
+++ 기능구현/회원가입/capston_sign_up.sql
@@ -1,7 +1,26 @@
 -- 1. 회원 가입 기능구현(아래 6개의 SQL은 동시에 실행되어야함. 
 -- 아래코드는 모두 시퀸스를 만든 후에야 정상작동 함으로 이를 유의할 것.
 
+-- (0) 이메일 인증하기(이메일이 인증된 상태여야 아래 회원가입이 작동하도록 만들 것)
+
+-- 인증번호 입력하기.
+INSERT INTO auth_codes (auth_id, user_email, auth_code, auth_purpose, expires_at)
+VALUES (seq_auth_codes.NEXTVAL, :email, :code, :purpose, :expiration_time);  
+
+-- ... 이메일 전송으로 보안코드 (백엔드)
+
+-- 인증번호 확인하기.
+select count(*) -- 결과가 1이면 인증완료
+from auth_codes
+WHERE user_email = :email -- 이메일이 일치하는지 
+AND auth_code = :code      -- 사용자에게 입력받을 코드
+AND is_used = 0             -- 인증코드 사용여부
+AND expires_at > SYSTIMESTAMP; -- 인증만료시간 안지났는지 체크
+
+
+
 BEGIN
+
 -- (1) user_t 테이블에 기본정보 삽입
 INSERT INTO users_t (
     user_id, user_name, user_email, user_password, user_age, user_gender,
@@ -31,6 +50,10 @@ INSERT INTO user_skill (user_id, skill_name) VALUES (users_seq.CURRVAL, :skill_n
 INSERT INTO user_activities ( activity_id, user_id, activity_type, activity_name, start_date, end_date, activity_description ) 
 VALUES (activities_seq.NEXTVAL, users_seq.CURRVAL, :activity_type, :activity_name, :start_date, :end_date, :activity_description );
 
+-- (7) 인증 테이블 업데이트
+update auth_codes set is_used = 1 where user_email = :email;
+
+
 commit; 
 
 EXCEPTION
@@ -38,4 +61,5 @@ EXCEPTION
         ROLLBACK;
         RAISE;
 END;
+
 
