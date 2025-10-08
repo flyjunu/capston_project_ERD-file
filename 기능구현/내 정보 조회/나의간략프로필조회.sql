002833e6ed6d6ad2dbbcea295ec467c0d42fb7ab
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-10-08 18:00:29 +0900

    Update 나의간략프로필조회.sql

--- 기능구현/내 정보 조회/나의간략프로필조회.sql
+++ 기능구현/내 정보 조회/나의간략프로필조회.sql
@@ -1,6 +1,7 @@
 -- (1) 메인화면에 나타날 나의 간략 프로필 
 
-select A.user_name AS 이름, A.user_grade AS 학년, B.job_name AS 희망직종
+select /*+ leading(A C B) index(A) index(C) index(B) use_nl(C) use_nl(B) */
+  A.user_name AS 이름, A.user_grade AS 학년, B.job_name AS 희망직종
 from users_t A, job_categories B, user_job_preferences C
 where A.user_id = :user_id
 and C.user_id = A.user_id
