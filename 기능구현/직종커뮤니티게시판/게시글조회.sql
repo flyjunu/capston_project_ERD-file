8c1726db631d45f41a9c554547993db43ca86df9
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-10-08 17:24:02 +0900

    Update 게시글조회.sql

--- 기능구현/직종커뮤니티게시판/게시글조회.sql
+++ 기능구현/직종커뮤니티게시판/게시글조회.sql
@@ -1,3 +1,5 @@
+
+-- (1) 게시글 목록보기
 SELECT
     jcb.post_id,
     jcb.post_title,
@@ -10,3 +12,13 @@ FROM
 WHERE jcb.job_id = :job_id 
 and jcb.user_id = u.user_id
 ORDER BY jcb.created_at DESC;
+
+-- (2) 조회수 증가
+UPDATE job_community_board
+   SET view_count = view_count + 1
+ WHERE post_id = :post_id;
+
+-- (3) 게시글 상세 정보 보기
+SELECT *
+  FROM job_community_board
+ WHERE post_id = :post_id;
