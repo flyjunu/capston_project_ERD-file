0afb91124ea4771c51dd84386a374ccd37defaa8
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-10-09 17:41:06 +0900

    Update 질문글상세조회.sql

--- 기능구현/취업QNA게시판/질문글상세조회.sql
+++ 기능구현/취업QNA게시판/질문글상세조회.sql
@@ -3,22 +3,15 @@ UPDATE qna_questions
    SET view_count = view_count + 1
  WHERE question_id = :question_id;
 
--- 2. 질문 상세 정보와 모든 답변 함께 가져오기
+-- 2. 질문 상세 정보와 모든 답변 함께 가져오기(따로만든 이유. 답변이 없을 수 있어서.)
 -- (질문 정보)
 SELECT * 
    FROM qna_questions 
    WHERE question_id = :question_id;
 -- (답변 목록)
-SELECT
-    a.answer_id,
-    a.answer_content,
-    u.user_name AS answerer_name,
-    a.created_at
-FROM
-    qna_answers a
-JOIN
-    users_t u ON a.user_id = u.user_id
-WHERE
-    a.question_id = :question_id
-ORDER BY
-    a.created_at ASC;
+SELECT a.answer_id,a.answer_content, u.user_name AS answerer_name, a.created_at
+FROM qna_answers a, users_t u
+WHERE a.user_id = u.user_id
+and a.question_id = :question_id
+ORDER BY a.created_at ASC;
+
