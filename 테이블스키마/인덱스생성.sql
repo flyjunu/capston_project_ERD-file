f8cd143d3a6b2ff9f2d9b598ed716732e5bd664d
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-10-09 17:52:32 +0900

    Update 인덱스생성.sql

--- 테이블스키마/인덱스생성.sql
+++ 테이블스키마/인덱스생성.sql
@@ -20,6 +20,7 @@ create index user_certificates_info_idx01 on user_certificates_info(user_id)
 create index user_departments_idx01 on user_departments(user_id);
 
 -- 9. 유저 기술 테이블
+create index user_skill_idx01 on user_skill(user_id);
 
 -- 10. 유저 대/내외 활동 내역 테이블
 create index user_activities_idx01 on user_activities(user_id);
@@ -38,12 +39,23 @@ CREATE INDEX activity_postings_idx01 ON activity_postings (activity_type, recrui
 -- 16. 직종별 게시판 테이블 
 CREATE INDEX job_community_board_idx01 ON job_community_board (job_id, created_at);
 -- 17. QnA 게시판(질문) 테이블 생성
+BEGIN
+  CTX_DDL.CREATE_SECTION_GROUP('qna_section_group', 'MULTI_COLUMN_DATASTORE');
+  CTX_DDL.ADD_FIELD_SECTION('qna_section_group', 'title', 'title');
+  CTX_DDL.ADD_FIELD_SECTION('qna_section_group', 'content', 'question_content');
+END;
+CREATE INDEX qna_questions_idx01 ON qna_questions(title)
+INDEXTYPE IS CTXSYS.CONTEXT
+PARAMETERS ('DATASTORE CTXSYS.MULTI_COLUMN_DATASTORE SECTION GROUP qna_section_group SYNC (ON COMMIT)');
+
+CREATE INDEX qna_questions_idx02 ON qna_questions(created_at);
 
 -- 18. QnA 게시판(답변) 테이블 생성
+CREATE INDEX qna_answers_idx02 ON  qna_answers(created_at);
 
 -- 19. 유저 비교 대학선택 저장 테이블
 
--- 20. 유저 프로핑 저장 테이블
+-- 20. 유저 프로필 저장 테이블
 
 
 
