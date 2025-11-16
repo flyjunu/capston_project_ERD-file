0d6d9322ec617a6ce89d98c5e12539306c042d5b
flyjunu <96277486+flyjunu@users.noreply.github.com>
2025-11-17 00:09:44 +0900

    Update 내 스펙 요약.sql

--- 기능구현/메인화면스펙비교/내 스펙 요약.sql
+++ 기능구현/메인화면스펙비교/내 스펙 요약.sql
@@ -10,8 +10,8 @@ FROM (SELECT u.user_id,NVL(c.cnt, 0) AS contest_count
 WHERE x.user_id = :user_id;
 
 -- 2. 자격증 백분율 조회
-SELECT cert_total_score,
-       ROUND(CUME_DIST() OVER (ORDER BY cert_total_score DESC) * 100, 1) AS upper_percent
+SELECT cert_total_score as 자격증 개수,
+       ROUND(CUME_DIST() OVER (ORDER BY cert_total_score DESC) * 100, 1) AS 자격증점수
 FROM (SELECT u.user_id, NVL(s.total_score, 0) AS cert_total_score
       FROM users_t u, (SELECT uci.user_id, SUM((CASE c.certificate_category
                                                 WHEN '기능사'   THEN 1
@@ -33,8 +33,8 @@ FROM (SELECT u.user_id, NVL(s.total_score, 0) AS cert_total_score
 WHERE x.user_id = :user_id;
 
 -- 3. 인턴경험 백분율 조회
-SELECT intern_count,
-       ROUND(CUME_DIST() OVER (ORDER BY intern_count DESC) * 100, 1) AS upper_percent
+SELECT intern_count as 인턴횟수,
+       ROUND(CUME_DIST() OVER (ORDER BY intern_count DESC) * 100, 1) AS 백분율
 FROM (SELECT u.user_id, NVL(i.cnt, 0) AS intern_count
       FROM users_t u, (SELECT ua.user_id, COUNT(*) AS cnt
                        FROM user_activities ua
@@ -44,11 +44,12 @@ FROM (SELECT u.user_id, NVL(i.cnt, 0) AS intern_count
 WHERE x.user_id = :user_id;
 
 -- 4. 활동 경험 백분율 조회 (인턴 뺴고 나머지 활동) 
-SELECT intern_count, ROUND(CUME_DIST() OVER (ORDER BY intern_count DESC) * 100, 1) AS upper_percent
-FROM (SELECT u.user_id, NVL(i.cnt, 0) AS intern_count
+SELECT activity_count as 활동횟수, 
+       ROUND(CUME_DIST() OVER (ORDER BY acticity_count DESC) * 100, 1) AS 백분율
+FROM (SELECT u.user_id, NVL(i.cnt, 0) AS activity_count
       FROM users_t u, (SELECT ua.user_id, COUNT(*) AS cnt
                        FROM user_activities ua
-                       WHERE ua.activity_type IN ('인턴', '인턴십')
+                       WHERE ua.activity_type NOT IN ('인턴', '인턴십')
                        GROUP BY ua.user_id) i
      WHERE u.user_id = i.user_id(+)) x
 WHERE x.user_id = :user_id;
